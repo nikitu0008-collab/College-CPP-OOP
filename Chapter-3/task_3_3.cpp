c584bf2414828403b99b25b0aa835fc916cff4b8
Niki-Zip <nikitu0008@gmail.com>
2026-03-13 19:43:20 +0300

    Add files via upload

--- Chapter-3/task_3_3.cpp
+++ Chapter-3/task_3_3.cpp
@@ -2,28 +2,34 @@
 #include <string>
 #include <vector>
 class Planet{
-    std::string name_planet;
-    unsigned int age_planet; 
-    int temperature;
+    unsigned int mass_, col_sputnik_;
+    std::string structure_atmosfer_, name_planet_;
+    float pressure_surface_;
     public:
     //*It's a construction and parametrs
-    Planet(std::string name_planet, unsigned int age_planet, int temperature){
-        this->name_planet = name_planet;
-        this->age_planet = age_planet;
-        this->temperature = temperature;
+    Planet(std::string name_planet_, unsigned int mass_, unsigned int col_sputnik_, std::string structure_atmosfer_, float pressure_surface_){
+        this->name_planet_ = name_planet_;
+        this->mass_ = mass_;
+        this->col_sputnik_ = col_sputnik_;
+        this->structure_atmosfer_ = structure_atmosfer_;
+        this->pressure_surface_ = pressure_surface_;
     }
     void printInfo(){
-        std::cout << "Planet: " <<name_planet << std::endl;
-        std::cout << "Age: " << age_planet << std::endl;
-        std::cout << "Temperature: " << temperature << std::endl;
+        std::cout << name_planet_ << ":" << std::endl;
+        std::cout << "      Mass: " << mass_ << std::endl;
+        std::cout << "      Structure atmosfer: " << structure_atmosfer_ << std::endl;
+        std::cout << "      Pressure surface: " << pressure_surface_ << std::endl;
+        std::cout << "      Col sputnik: " << col_sputnik_ << std::endl;
     }
 };
 int main(){
-    Planet p1("Earth",2500,25);
-    Planet p2("Neptun",12345,-50);
-    Planet p3("Mars", 54321, -500);
+    Planet p1("Earth", 0, 1, "A, A", 2.22);
+    Planet p2("Neptun", 10, 5, "Azot, Amogus", 1.23);
+    Planet p3("MyPensil", 9999, 5, "Diamond, Beef, SUS", 1000);
+    Planet p4("Lyna", 1023, 5, "Lyni", 1);
     p1.printInfo();
     p2.printInfo();
     p3.printInfo();
+    p4.printInfo();
     return EXIT_SUCCESS;
 }
