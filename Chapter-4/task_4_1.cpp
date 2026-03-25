d88f58783fe6e0cd46a308bfab1f6b048f88c468
Niki-Zip <nikitu0008@gmail.com>
2026-03-25 16:51:02 +0300

    Add files via upload

--- Chapter-4/task_4_1.cpp
+++ Chapter-4/task_4_1.cpp
@@ -1,38 +1,40 @@
 #include <iostream>
+#include <stdexcept> //* std::invalid_argument
 #include <string>
 #include <vector>
 #include <print>
+#include <cstdint> //* uint(8, 16, 32)_t
+#include <cstdlib>
 class Bed{
-    private:
+    //*Inisialization
+    public:
+    Bed();
+    auto printInformation() -> void;
+    auto redactorBed() -> void;
+    protected:
     bool two_tier_;
     unsigned int col_places_;
     std::string material_, two_tier_anwer_;
     float seller_;
-    public:
-        Bed() : two_tier_(false), col_places_(0), material_("unknown"), seller_(0.0), two_tier_anwer_("unknown") {}
-        Bed(bool two_tier, unsigned int col_places, std::string material, float seller, std::string two_tier_anwer){
-            this->two_tier_ = two_tier;
-            this->col_places_ = col_places;
-            this->material_ = material;
-            this->seller_ = seller;
-            this->two_tier_anwer_ = two_tier_anwer;
-        }
-        auto printInformation() -> void{
-            std::println("\nBed:");
-            std::println("      two yars: {}",two_tier_);
-            std::println("      col places: {}",col_places_);
-            std::println("      material: {}",material_);
-            std::println("      seller: {}",seller_);
-        }
-        auto redactorBed() -> void{
-            std::println("Two yars? [Y/n] >_:"); std::cin >> two_tier_anwer_; if(two_tier_anwer_ == "y" || two_tier_anwer_ == "Y"){two_tier_=true;}
-            std::println("col_places: "); std::cin >> col_places_;
-            std::println("material: "); std::cin >> material_;
-            std::println("seller: "); std::cin >> seller_;
-        }
 };
+
+Bed::Bed() : two_tier_(false), col_places_(0), material_("unknown"), seller_(0.0){}
+
+auto Bed::printInformation() -> void{
+    std::println("\nBed:");
+    std::println("      two yars: {}",two_tier_);
+    std::println("      col places: {}",col_places_);
+    std::println("      material: {}",material_);
+    std::println("      seller: {}",seller_);
+}
+auto Bed::redactorBed() -> void{
+    std::println("Two yars? [Y/n] >_:"); std::cin >> two_tier_anwer_; if(two_tier_anwer_ == "Y" || two_tier_anwer_ == "y") { two_tier_ = true; }
+    std::println("col_places: "); std::cin >> col_places_;
+    std::println("material: "); std::cin >> material_;
+    std::println("seller: "); std::cin >> seller_;
+}
 auto main() -> int{
-    unsigned int col_bad, answer;
+    unsigned int col_bad = 0, answer = 0;
     std::println("col bad: ");
     std::cin >> col_bad;
     std::vector<Bed> bed(col_bad);
@@ -40,20 +42,20 @@ auto main() -> int{
     std::cin >> answer;
     switch(answer){
         case 1:
-        for(int i = 0 ; i < bed.size() ; i++){
-            bed.at(i).redactorBed();
+        for(size_t i = 0 ; i<bed.size() ; i++){
+            bed[i].redactorBed();
         }
-        for(int i = 0 ; i < bed.size() ; i++){
+        for(size_t i = 0 ; i<bed.size() ; i++){
             bed[i].printInformation();
         }
         break;
         case 2:
-        for(int i = 0 ; i < bed.size() ; i++){
-            bed.at(i).printInformation();
+        for(size_t i = 0 ; i<bed.size() ; i++){
+            bed[i].printInformation();
         }
         break;
-        default: 
-        throw std::invalid_argument("Invalid signal value"); //*Exception
+        default:
+        throw std::invalid_argument("Invalid signal value");
         break;
     }
     return EXIT_SUCCESS;
