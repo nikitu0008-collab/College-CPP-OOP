56f0dbf4796a8a6510e9ff5938e75a8340ced07c
NikiZip <nikitu0008@gmail.com>
2026-04-03 08:38:36 +0300

    Add files via upload

--- Chapter-4/task_4_1.cpp
+++ Chapter-4/task_4_1.cpp
@@ -4,11 +4,12 @@
 #include <vector>
 #include <print>
 #include <cstdlib>
+#include <cstdint> //int8..16.._t
 class Bed{
     //*Inisialization
     public:
     Bed();
-    auto printInformation() -> void;
+    auto printInformation() noexcept -> void;
     auto redactorBed() -> void;
     protected:
     bool two_tier_;
@@ -19,7 +20,7 @@ class Bed{
 
 Bed::Bed() : two_tier_(false), col_places_(0), material_("unknown"), seller_(0.0){}
 
-auto Bed::printInformation() -> void{
+auto Bed::printInformation() noexcept -> void{
     std::println("\nBed:");
     std::println("      two yars: {}",two_tier_);
     std::println("      col places: {}",col_places_);
@@ -27,13 +28,14 @@ auto Bed::printInformation() -> void{
     std::println("      seller: {}",seller_);
 }
 auto Bed::redactorBed() -> void{
-    std::println("Two yars? [Y/n] >_:"); std::cin >> two_tier_anwer_; if(two_tier_anwer_ == "Y" || two_tier_anwer_ == "y") { two_tier_ = true; }
+    std::println("Two yars? [Y/n] >_:"); std::cin >> two_tier_anwer_; (two_tier_anwer_ == "Y" || two_tier_anwer_ == "y") ? two_tier_ = true : false;
     std::println("col_places: "); std::cin >> col_places_;
     std::println("material: "); std::cin >> material_;
     std::println("seller: "); std::cin >> seller_;
 }
 auto main() -> int{
-    unsigned int col_bad = 0, answer = 0;
+    unsigned int col_bad = 0; 
+    int8_t answer = 0;
     std::println("col bad: ");
     std::cin >> col_bad;
     std::vector<Bed> bed(col_bad);
