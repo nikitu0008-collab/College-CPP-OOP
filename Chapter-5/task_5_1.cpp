7f485f8afddab827e7ee43b08f101319d270ddc7
NikiZip <nikitu0008@gmail.com>
2026-04-21 11:12:48 +0300

    Add files via upload

--- Chapter-5/task_5_1.cpp
+++ Chapter-5/task_5_1.cpp
@@ -1,75 +1,73 @@
-#include <stdexcept> 
+#include <cstdlib>
 #include <iostream>
 #include <print>
-#include <cstdlib>
-#include <vector>
+#include <stdexcept>
 #include <string>
-class Plate{
-    public:
-    //*It's a constructor by default
-    Plate() : hight_(0.0), width_(0.0), cost_(0.0), weidth_(0.0), seller_(0.0), material_("unknown"), manufacturer_("unknown"){} 
-    
-    //*It's constructor full parametrs
-    Plate(float hight, float width, float cost, float weidth, float seller, std::string material, std::string manufacturer) : hight_(hight), width_(width), cost_(cost), weidth_(weidth), seller_(seller), material_(material), manufacturer_(manufacturer){}
-    
-    auto printInformation() const noexcept -> void {
-        std::println("Plate is a {}, size(w, h, c): {}, {}, {}; weidth: {}, seller: {}, manufacturer: {}", material_,  width_, hight_, cost_, weidth_, seller_, manufacturer_);
-    }
-    ~Plate() { 
-        std::println("Bed manufacturer: {}, Deleted", manufacturer_); 
-    }
+#include <vector>
+#include <cstdint>
+class Plate {
+public:
+  //*It's a constructor by default
+  Plate() : hight_(0.0), width_(0.0), cost_(0.0), weidth_(0.0), seller_(0.0), material_("unknown"), manufacturer_("unknown") {}
+  Plate(float hight, float width, float cost, float weidth, float seller, std::string material, std::string manufacturer) : hight_(hight), width_(width), cost_(cost), weidth_(weidth), seller_(seller), material_(material), manufacturer_(manufacturer) {}
 
-    protected:
-    float hight_, width_, cost_, weidth_, seller_;
-    std::string material_, manufacturer_;
+  constexpr auto printInformation() const noexcept -> void {
+    std::println("Plate is a {}, size(w, h, c): {}, {}, {}; weidth: {}, seller: {}, manufacturer: {}",material_, width_, hight_, cost_, weidth_, seller_,manufacturer_);
+  }
+private:
+  float hight_, width_, cost_, weidth_, seller_;
+  std::string material_, manufacturer_;
 };
 auto main() -> int {
-    unsigned int index = 0, answer = 0;
+  uint16_t index = 0, answer = 0;
 
-    //*Parametrs new add Plate
-    float hight_new, width_new, cost_new, seller_new, weidth_new;
-    std::string material_new, manufacturer_new;
+  //*Parametrs new add Plate
+  //*It's constructor full parametrs
+  float hight_new = 0, width_new = 0, cost_new = 0, seller_new = 0, weidth_new = 0;
+  std::string material_new, manufacturer_new;
 
-    std::vector<Plate> plate;
-    plate.emplace_back(1, 2, 3, 4, 5, "A", "B");
-    plate.emplace_back(100, 50, 1000, 12, 33, "SUS", "SUGOMA");
-    for(Plate const& i : plate){
+ std::vector<Plate> plate;
+  plate.emplace_back(10, 25, 30, 45, 15, "A", "K");
+  plate.emplace_back(100, 50, 1000, 12, 33, "SUS", "SUGOMA");
+  plate.emplace_back(50, 5, 50, 100, 30, "plitO4ka", "O4ken");
+  for (const Plate& i : plate) {
+    i.printInformation();
+  }
+  while (answer != 3) {
+    //*using const to avoid changing the given class of the object
+    std::println("\n\t1.add plate\n\t2.delete plate\n\t3.exit\n:_>");
+    std::cin >> answer;
+    switch (answer) {
+    case 1:
+      system("clear");
+      std::println("Enter name and full parametrs. (height, width, cost, weidth, seller, material)");
+      std::cin >> hight_new >> width_new >> cost_new >> weidth_new >> seller_new >> material_new >> manufacturer_new;
+      //*clangd recomended using emplace_back instead of push_back.
+      plate.emplace_back(hight_new, width_new, cost_new, weidth_new, seller_new, material_new, manufacturer_new);
+      for (const Plate& i : plate) {
+          i.printInformation();
+      }
+      break;
+    case 2:
+      std::println("enter index: ");
+      std::cin >> index;
+      //*checking user don't enter index >= plate.size()
+      if (index >= plate.size()) {
+        throw std::invalid_argument("index > size vector");
+      }
+      plate.erase(plate.begin() + index);
+      system("clear");
+      for (const Plate& i : plate) {
         i.printInformation();
+      }
+      break;
+    case 3:
+      //*call destructor
+      break;
+    default:
+      throw std::invalid_argument("Errors choice");
+      break;
     }
-    while(answer != 3){ //*clangd recomended don't using do-while. That's why i'm using cycles while
-
-        //*using const to avoid changing the given class of the object
-        std::println("\n\t1.add plate\n\t2.delete plate\n\t3.exit\n:_>");
-        std::cin >> answer;
-        switch(answer){
-            case 1:
-            std::println("Enter full parametrs. (height, width, cost, weidth, seller, material)");
-            std::cin >>  hight_new >> width_new >> cost_new >> weidth_new >> seller_new >> material_new >> manufacturer_new;
-            //*clangd recomended using emplace_back instead of push_back.
-            plate.emplace_back(hight_new,width_new,cost_new,weidth_new,seller_new,material_new,manufacturer_new);
-            for(const Plate& i : plate){
-                i.printInformation();
-            }
-            break;
-            case 2:
-            std::println("enter index: ");
-            std::cin >> index;
-            //*checking user don't enter index > plate.size()
-            if(index > plate.size() || index < 0){
-                throw std::invalid_argument("index > size vector");
-            }
-            plate.erase(plate.begin() + index);
-            for(const Plate& i: plate){
-                i.printInformation();
-            }
-            break;
-            case 3:
-            //*Call destruction.
-            break;
-            default:
-            throw std::invalid_argument("Errors choice");
-            break;
-        }
-    }
-    return EXIT_SUCCESS;
+  }
+  return EXIT_SUCCESS;
 }
--- Chapter-5/task_5_2.cpp
+++ Chapter-5/task_5_2.cpp
@@ -3,11 +3,13 @@
 #include <iostream>
 #include <utility> //* std::unreachable()
 #include <vector>
+#include <stdexcept>
 #include <string>
+#include <cstdint>
 class Worker{
     public:
     //*constructor standart
-    Worker() :id_{0}, age_{0}, name_{"unknown-name"}, gender_{"unknown-gen"}, position_{"unknown-pos"}, department_{"unknown-dep"} {}
+    Worker() : id_(0), age_(0), name_("unknown-name"), gender_("unknown-gen"), position_("unknown-pos"), department_("unknown-dep") {}
     
     //*delegating constructor
     Worker(unsigned int id) : Worker() {id_ = id;}
@@ -15,31 +17,26 @@ class Worker{
     Worker(unsigned int id, unsigned int age, std::string gender) : Worker(id, age) {gender_ = gender;}
     Worker(unsigned int id, unsigned int age, std::string gender, std::string department) : Worker(id, age, gender) {department_ = department;}
     
-    auto  printWorker() noexcept ->  void {
+    auto constexpr printWorker() const noexcept ->  void {
         std::println("Name: {}\nID:{}\nAge:{}\nGender:{}\nPosition:{}\nDepartment:{}", name_, id_, age_, gender_, position_, department_);
     }
     auto redactorWorker() -> void{
-        unsigned int id_redactor;
+        uint16_t id_redactor;
         std::println("Enter ID redactor: ");
         std::cin >> id_redactor;
         std::println("Enter redactor new: ID, Age, Name, Gender, Position, Department");
-        //? What Error's.
         std::cin >> id_ >> age_ >> name_ >> gender_ >> position_ >> department_;
     }
     auto addWorker() noexcept -> void{
         std::println("Enter New(ID, Age, Name, Gender, Position, Department)");
         std::cin >> id_ >> age_ >> name_ >> gender_ >> position_ >> department_;
     }
-    ~Worker(){
-        std::println("Worker [{}]{} deleted..",id_,  name_);
-    }
-    //*Thanks to protected, you can access them in inheritance
-    protected:
-    unsigned int id_, age_;
+    private:
+    uint16_t id_, age_;
     std::string name_, gender_, position_, department_;
 };
 //*Static function
-auto emptyVector(std::vector<Worker>& worker) -> void {
+auto emptyVector(std::vector<Worker>& worker) noexcept -> void {
     if(worker.empty()){
         std::println("vector worker empty");
         return;
@@ -47,20 +44,20 @@ auto emptyVector(std::vector<Worker>& worker) -> void {
 }
 auto main() -> int {
     std::vector<Worker> worker;
-    unsigned int answer = 0;
-    int id_delete;
+    uint16_t answer = 0, id_delete = 0;
     bool answer_num = false;
     while(answer != 5){
         std::print("Worker:\n\t1.print\n\t2.redactor\n\t3.add\n\t4.delete\n\t5.exit\n:_>");
         std::cin >> answer;
-        (answer > 0 && answer < 6) ? answer_num = true : false;
+        if(answer > 0 && answer < 6){ answer_num = true; }
+        
         if(!answer_num){
-            std::cerr << "Error answer" << std::endl;
+            throw std::invalid_argument("Error answer");
         } 
         switch(answer){
             case 1: 
             emptyVector(worker);
-            for(Worker& i : worker){i.printWorker();} 
+            for(const Worker& i : worker){i.printWorker();} 
             break;
             case 2: 
             emptyVector(worker);for(Worker& i : worker){i.redactorWorker();} 
@@ -74,13 +71,12 @@ auto main() -> int {
             case 4: 
             emptyVector(worker); 
             std::println("Enter index: "); std::cin >> id_delete; 
-            if(id_delete < 0 || id_delete > worker.size()){ 
-                std::println("index error!");
-                continue;
+            if(id_delete >= worker.size()){ 
+                throw std::invalid_argument("Index error");
             }
             worker.erase(worker.begin() + id_delete); 
             break;
-            case 5: /*Destruction*/ break;
+            case 5: std::println("Bye.."); break;
             default: std::unreachable(); //*saying compiler that we definitely won't get here.
         }
     }
