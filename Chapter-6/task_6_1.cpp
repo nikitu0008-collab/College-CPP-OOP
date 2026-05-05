83b959bf4d3480ee1bff9233cd0f8ae02b4c4166
NikiZip <nikitu0008@gmail.com>
2026-05-05 11:21:39 +0300

    Add files via upload

--- Chapter-6/task_6_1.cpp
+++ Chapter-6/task_6_1.cpp
@@ -1,94 +1,77 @@
 #include <algorithm>
 #include <cstdlib>
-#include <functional>
 #include <iostream>
 #include <print>
-#include <stdexcept> //* std::invalid_argument
-#include <utility> //* std::unreachable
 #include <vector>
 #include <string>
 class Planet {
 public:
-    Planet(std::string name_planet, unsigned int mass, unsigned int col_sputnik, std::string structure_atmosfer, float pressure_surface) : mass_(mass), col_sputnik_(col_sputnik), structure_atmosfer_(structure_atmosfer), name_planet_(name_planet), pressure_surface_(pressure_surface) {}
-    //*Сеттеры
-    auto setFn(std::string name_planet) -> void { name_planet_ = name_planet; }
-    auto setFn2(unsigned int mass) -> void { mass_ = mass; }
-    auto setFn3(unsigned int col_sputnik) -> void { col_sputnik_ = col_sputnik; }
-    auto setFn4(std::string structure_atmosfer) -> void { structure_atmosfer_ = structure_atmosfer; }
-    auto setFn5(float pressure_surface) -> void { pressure_surface_ = pressure_surface; }
+    Planet(){};
+    Planet(unsigned int mass, unsigned int col_sputnik, std::string structure_atmosfer, std::string name_planet, double pressure_surface) : mass_(mass), col_sputnik_(col_sputnik), structure_atmosfer_(structure_atmosfer), name_planet_(name_planet), pressure_surface_(pressure_surface) {}
     
-    //*Геттеры
-    /*nodiscard - означает что функция не предназначена для удаления*/
-    [[nodiscard]] auto getFName() const -> std::string { return name_planet_; }
-    [[nodiscard]] auto getFMass() const -> unsigned int { return mass_; }
-    [[nodiscard]] auto getFColS() const -> unsigned int { return col_sputnik_; }
-    [[nodiscard]] auto getFSA() const -> std::string { return structure_atmosfer_; }
-    [[nodiscard]] auto getFPS() const -> float { return pressure_surface_; }
+    auto setNamePlanet(const std::string& name_planet)->Planet&{ name_planet_ = name_planet; return *this;}
+    auto setMass(unsigned int mass)->Planet& { mass_ = mass; return *this;}
+    auto setColSputnik(unsigned int col_sputnik)->Planet&{ col_sputnik_ = col_sputnik; return *this;}
+    auto setStructureAtmosfer(const std::string& structure_atmosfer)->Planet&{ structure_atmosfer_ = structure_atmosfer; return *this;}
+    auto setPressureSurface(double pressure_surface)->Planet&{ pressure_surface_ = pressure_surface; return *this; }
+
+    [[nodiscard]] auto getName() const -> const std::string& { return name_planet_; }
+    [[nodiscard]] auto getMass() const -> unsigned int { return mass_; }
+    [[nodiscard]] auto getColSputnik() const -> unsigned int { return col_sputnik_; }
+    [[nodiscard]] auto getStructureAtmosfer() const -> const std::string& { return structure_atmosfer_; }
+    [[nodiscard]] auto getPressureSurface() const -> double { return pressure_surface_; }
     
-    auto printInfo() const noexcept -> void {
-      std::println("[1]{}", name_planet_);
-      std::println("      [2]Mass: {}", mass_);
-      std::println("      [3]Structure atmosfer: {}", structure_atmosfer_);
-      std::println("      [4]Pressure surface: {}", pressure_surface_);
-      std::println("      [5]Col sputnik: {}", col_sputnik_);
+    auto constexpr printInfo() const noexcept -> void {
+      std::println("1.{}", name_planet_);
+      std::println("      2.Mass: {}", mass_);
+      std::println("      3.Structure atmosfer: {}", structure_atmosfer_);
+      std::println("      4.Pressure surface: {}", pressure_surface_);
+      std::println("      5.Col sputnik: {}", col_sputnik_);
     }
 
 private:
     unsigned int mass_, col_sputnik_;
     std::string structure_atmosfer_, name_planet_;
-    float pressure_surface_;
+    double pressure_surface_;
 };
 auto main() -> int {
     unsigned int field = 0;
     std::vector<Planet> p = {
-        Planet("Earth", 0, 1, "A", 2.22),
-        Planet("Neptun", 30, 5, "Amogus", 1.23),
-        Planet("MyPensil", 3, 5, "SUS", 1000),
-        Planet("Lyna", 10, 5, "L", 1),
+       Planet(350000,50,"Apple","NplDig",25.66),
+       Planet(150900,1000, "Pensil", "AAME",211.12),
+       Planet(200200, 20, "Gagaga", "Pepa", 100.24),
+       Planet(600800, 100, "PEPE", "Shnene", 10.23),
     };
-    for (const Planet &i : p) {
+    for (const Planet& i : p) {
       i.printInfo();
     }
-
     std::print("Enter field to sort by: ");
     std::cin >> field;
-    if (field < 1 or field > 5) {
-      throw std::invalid_argument("Error's field");
+    for(size_t i = 0 ; i < p.size() - 1 ; i++){
+        for(size_t j = 0 ; j < p.size() - i - 1 ; j++){
+            switch(field){
+                case 1:
+                if(p.at(j).getName() > p.at(j+1).getName()){std::swap(p.at(j), p.at(j+1));}
+                break;
+                case 2:
+                if(p.at(j).getMass() > p.at(j+1).getMass()){std::swap(p.at(j), p.at(j+1));}
+                break;
+                case 3:
+                if(p.at(j).getColSputnik() > p.at(j+1).getColSputnik()){std::swap(p.at(j), p.at(j+1));}
+                break;
+                case 4:
+                if(p.at(j).getPressureSurface() > p.at(j+1).getPressureSurface()){std::swap(p.at(j), p.at(j+1));}
+                break;
+                case 5:
+                if(p.at(j).getColSputnik() > p.at(j+1).getColSputnik()){std::swap(p.at(j), p.at(j+1));}
+                break;
+                default:
+                std::cerr << "Error answer" << std::endl;
+        }
     }
-    switch (field) {
-    //*std::ranges::less stable_sort
-    case 1:
-    std::ranges::stable_sort(p, std::ranges::less{}, &Planet::getFName);
     for(const Planet& i : p){
-      i.printInfo();
-    }
-    break;
-    case 2:
-    std::ranges::stable_sort(p, std::ranges::less{}, &Planet::getFMass);
-    for(const Planet& i : p){
-      i.printInfo();
-    }
-    break;
-    case 3:
-    std::ranges::stable_sort(p, std::ranges::less{}, &Planet::getFSA);
-      for(const Planet& i : p){
         i.printInfo();
-      }
-    break;
-    case 4:
-      std::ranges::stable_sort(p, std::ranges::less{}, &Planet::getFPS);
-      for(const Planet& i : p){
-        i.printInfo();
-      }
-      break;
-    case 5:
-      std::ranges::stable_sort(p, std::ranges::less{}, &Planet::getFColS);
-      for(const Planet& i : p){
-        i.printInfo();
-      }
-    break;
-      default:
-      std::unreachable(); //*saying compiler that we definitely won't get here.
     }
     return EXIT_SUCCESS;
+}
 }
--- Chapter-6/task_6_2.cpp
+++ Chapter-6/task_6_2.cpp
@@ -1,121 +1,138 @@
 #include <cstdlib>
-#include <stdexcept>
 #include <string>
 #include <print>
 #include <iostream>
 #include <vector>
 #include <cstdint>
-
+#include <exception>
 class Company{
     public:
     //*Construction
-    Company(std::string& answer_state, std::string& name,unsigned int income,unsigned int expenses) : answer_state_(answer_state), name_(name), income_(income), expenses_(expenses) {}
+    Company(std::string answer_state, std::string name,unsigned int income,unsigned int expenses) : answer_state_(answer_state), name_(name), income_(income), expenses_(expenses) {}
     Company() : answer_state_("unknown-state"), name_{"unknown-name"}, income_(0), expenses_(0) {}
 
     //*Get
-    [[nodiscard]] auto getFState() const -> std::string { return answer_state_; }
-    [[nodiscard]] auto getFName() const -> std::string { return name_; }
-    [[nodiscard]] auto getFIncom() const -> unsigned int { return income_; }
-    [[nodiscard]] auto getFExpenses() const -> unsigned int { return expenses_; }
+    [[nodiscard]] auto getState() const -> const std::string& { return answer_state_; }
+    [[nodiscard]] auto getName() const -> const std::string& { return name_; }
+    [[nodiscard]] auto getIncom() const -> unsigned int { return income_; }
+    [[nodiscard]] auto getExpenses() const -> unsigned int { return expenses_; }
     
     //*Set. I'm creating optimal setter, recomended from teacher
-    auto setFs(std::string& answer_state) -> Company& { answer_state_ = answer_state; return *this;}
-    auto setFn(std::string& name) -> Company& { name_ = name; return *this;}
-    auto setFi(unsigned int income) -> Company& { income_ = income; return *this;}
-    auto setFe(unsigned int expenses) -> Company& { expenses_ = expenses; return *this;}
+    auto setState(std::string& answer_state) -> Company& { answer_state_ = answer_state; return *this;}
+    auto setName(std::string& name) -> Company& { name_ = name; return *this;}
+    auto setIncome(unsigned int income) -> Company& { income_ = income; return *this;}
+    auto setExpenses(unsigned int expenses) -> Company& { expenses_ = expenses; return *this;}
 
     auto redactorCompany() -> void {
         std::print("Enter new name, income and expenses: ");
         std::cin >> name_ >> income_ >> expenses_;
+        try{
+        if(income_ <= 0 or expenses_ <= 0) { throw "Income or expenses <= 0"; }
+        }catch(const std::exception& e){
+            std::println("{}", e.what());
+        }
     }
 
     private:
         std::string answer_state_, name_;
         unsigned int income_, expenses_;
 };
-static auto emptyVector(const std::vector<Company>& c) -> bool{
-    return c.empty();
-}
+
 auto main() -> int {
     uint16_t answer = 0, index = 0;
     int result = 0;
     std::string answer_state, name; 
     unsigned int income = 0, expenses = 0;
     std::vector<Company> company = {
-        //Если надо добавить какие-то значение изначально то ок.
-        //Пока что незнаю.
+        Company("private", "Shnele", 499999, 1000875),
+        Company("state", "AGY-AGY", 1000, 190002),
+        Company("private", "NVIM", 11, 11),
+        Company("state","A",5000001,5000010)
     };
     while(answer != 6){
         std::print("\n\t1.Print   2.RedactorCompany\n\t3.AddCompany    4.Delete Company\n\t5.Calculation   6.Exit\n:_<");
         std::cin >> answer;
         switch(answer){
             case 1:
-            if(emptyVector(company)){ std::println("vector empty"); continue; }
+            if(company.empty()){ std::println("vector empty"); continue; }
             for(const Company& i : company){
-                if(i.getFState() == "private" and i.getFIncom() < 5000000){
-                    std::println("\nStatus: {}\nName-hidden\nIncome: {}\nExpeses: {}", i.getFState(), i.getFIncom(), i.getFExpenses());
-                } else if(i.getFState() == "state"){
-                    if(i.getFIncom() < 5000000){
-                        std::println("\nName company: {}\n Income: hidden\nExpeses: hiddden",i.getFName());        
-                    } else if(i.getFIncom() >= 5000000){
-                            std::println("\nName company: {}\nIncome: {}\nExpenses: {}", i.getFName(), i.getFIncom(), i.getFExpenses());
+                if(i.getState() == "private" and i.getIncom() < 5000000){
+                    std::println("\nStatus: {}\nName-hidden\nIncome: {}\nExpeses: {}", i.getState(), i.getIncom(), i.getExpenses());
+                } else if(i.getState() == "private" and i.getIncom() > 499999){
+                    std::println("\nStatus: {}\nName: {}\nIncome: {}\nExpeses: {}", i.getState(), i.getName(), i.getIncom(), i.getExpenses());
+                } else if(i.getState() == "state"){
+                    if(i.getIncom() < 5000000){
+                        std::println("\nStatus: {}\nName company: {}\n Income: hidden\nExpeses: hiddden", i.getState(),i.getName());        
+                    } else if(i.getIncom() >= 5000000){
+                            std::println("\nStatus: {}\nName company: {}\nIncome: {}\nExpenses: {}", i.getState(), i.getName(), i.getIncom(), i.getExpenses());
                         }
-                } else if(i.getFState() == "unknown-state"){
-                    throw std::invalid_argument("Error state unknown");
+                } else if(i.getState() == "unknown-state"){
+                        std::cerr << "unknown-state" << std::endl;
+                        continue;
                     } else {
-                        throw std::invalid_argument("Error state");
+                        std::cerr << "Error state" << std::endl;
+                        continue;
                     }
             }
             break;
             case 2:
-            if(emptyVector(company)){std::println("vector empty"); continue; }
+            if(company.empty()){std::println("vector empty"); continue; }
             std::print("Enter index redactor: ");
             std::cin >> index;
             if(index >= company.size()){
-                throw std::invalid_argument("Error index");
+                std::cerr << "Error index" << std::endl;
+                continue;
             }
             company.at(index).redactorCompany();
             break;
             case 3:
-            std::println("state or private company?\n:_>");
+            try {
+            std::println("state or private company?");
             std::cin >> answer_state;
-            if(answer_state == "State" or answer_state == "state"){
+            } catch (const std::exception& e) {
+                std::cerr << "Error: " << e.what() << std::endl;
+                break;
+            }
+            if (answer_state == "State" or answer_state == "state") {
                 answer_state = "state";
-            } else if(answer_state == "Private" or answer_state == "private"){
-                    answer_state = "private";    
-                } else {
-                        answer_state = "unknown-state";
-                    }
-            std::println("Enter adding(name, income, expenses)");
+            } else if (answer_state == "Private" or answer_state == "private") {
+                answer_state = "private";
+            } else {
+                answer_state = "unknown-state";
+            }
+
+            std::println("Enter name, income, expenses:");
             std::cin >> name >> income >> expenses;
+
             company.emplace_back(answer_state, name, income, expenses);
             break;
             case 4:
-            if(emptyVector(company)){std::println("vector empty"); continue; }
+            if(company.empty()){std::println("vector empty"); continue; }
             std::print("Enter index: ");
             std::cin >> index;
             if(index >= company.size()){
-                throw std::invalid_argument("Error index");
+                std::cerr << "Error index" << std::endl;
+                continue; 
             }
             company.erase(company.begin() + index);
             break;
             case 5:
-            if(emptyVector(company)){std::println("vector empty"); continue; }
+            if(company.empty()){std::println("vector empty"); continue; }
             for(const Company& i : company){
-                if(i.getFState() == "state"){
+                if(i.getState() == "state"){
                     //forbidden -- запрещено(перевод)
                     std::println("Calculation forbidden");
                     continue;
                 }
                 //clangd recomendent const int result. Но я не хочу, потому что результат всё равно будет менятся при новых знач.
-                result = i.getFIncom() - i.getFExpenses(); 
-                std::println("Company: {}, calculation: {}", i.getFName(), result);       
+                result = i.getIncom() - i.getExpenses(); 
+                std::println("Company: {}, calculation: {}", i.getName(), result);       
             }
             break;
             case 6:
             break;
             default:
-            throw std::invalid_argument("Error default");
+                std::cerr << "Error answer" << std::endl;
             }
         }
     return EXIT_SUCCESS;
