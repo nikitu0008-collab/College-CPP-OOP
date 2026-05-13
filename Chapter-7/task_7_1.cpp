2435996d31ce2da6f26610355a9332dfbe04db7e
NikiZip <nikitu0008@gmail.com>
2026-05-13 09:05:10 +0300

    Add files via upload
    in corrected error

--- Chapter-7/task_7_1.cpp
+++ Chapter-7/task_7_1.cpp
@@ -2,30 +2,87 @@
 #include <cstdlib>
 #include <vector>
 #include <cstdint>
+#include <iostream>
+
 class Test{
     public:
     explicit Test() : number_(0){
-        std::println("this: {}",(void*)this);
+        std::cout << this << std::endl;
+        //std::println("this: {}",(void*)this);
     }
     Test(uint16_t number) : number_(number){
-        std::println("this: {}",(void*)this);
+        std::cout << this << std::endl;
+        //std::println("this: {}",(void*)this);
     }
     Test(const Test& obj) : number_(obj.number_){
-        std::println("object: {}, this: {}",(void*)&obj, (void*)this);
+        std::cout << "object: " << &obj << " this: " << this << std::endl;
+        //std::println("object: {}, this: {}",(void*)&obj, (void*)this);
     }
     auto info()->void{
-        std::println("this: {}, number: {}", (void*)this, number_);
+        std::cout << "this: " << this << " number: " << number_ << std::endl;
+        //std::println("this: {}, number: {}", (void*)this, number_);
     }
     private:
     uint16_t number_;
 };
 
 auto main()->int{
+    std::println("Object integer");
+    Test t1(1);
+    std::println("==================="); 
+    std::println("Creating object standart");
+    Test t2;
+    std::println("===================");
+    std::println("Creating t3(t1)");
+    
+    Test t3(t1);
+    
     std::vector<Test> test;
+    
+    std::println("=====VECTOR=====");
+    
+    std::println("emplace 67");
     test.emplace_back(67);
-    test.emplace_back();
-    test.at(0).info();
-    test.at(1).info();
+    for(Test& i : test){
+        i.info();
+    }
+   
+    std::println("==================");
+    
+    std::println("add 11 pushback");
+    test.push_back(t1);
+    for(Test& i : test){
+        i.info();
+    }
+    std::println("==================");
+    
+    std::println("erase");
+    test.erase(test.begin() + 0);
+    for(Test& i : test){
+        i.info();
+    }
+    std::println("==Emplace 3 numbers==");
+    
+    test.emplace_back(100);
+    test.emplace_back(69);
+    test.emplace_back(3);
+    for(Test& i : test){
+        i.info();
+    }
+    std::println("3 emplace_Back");
+    
+    std::println("=================");
+    
+    std::println("erase 1");
+    test.erase(test.begin() + 0);
+    
+    if(test.empty()){
+        std::println("Vector empty");
+    }
+    for(Test& i : test){
+        i.info();
+    }
+    std::println("=================");
 
     return EXIT_SUCCESS;
 }
