36ebdad5b710eae41357e78e6c87940f8b138089
NikiZip <nikitu0008@gmail.com>
2026-06-02 10:09:04 +0300

    Add files via upload

--- Chapter-9/task_9_2.cpp
+++ Chapter-9/task_9_2.cpp
@@ -7,7 +7,7 @@ class MyArray{
     MyArray() : size_(0), arr_(nullptr){}
 
     //Constructor int number
-    explicit MyArray(uint16_t number) : size_(number), arr_(new uint16_t[size_]){
+    explicit MyArray(int number) : size_(number), arr_(new int[size_]){
         std::println("===FILLING ARR===");
         for(size_t i = 0 ; i < size_ ; i++){ arr_[i] = 0; }
     }
@@ -17,12 +17,11 @@ class MyArray{
     
     //Increment, (Increment == ++)
     auto operator++(int) -> MyArray&{
-        const uint16_t new_size_ = size_ + 1;
-        uint16_t* new_arr_ = new uint16_t[new_size_];
+        const int new_size_ = size_ + 1;
+        int* new_arr_ = new int[new_size_];
         for(size_t i = 0 ; i < size_ ; i++){
             new_arr_[i] = arr_[i];
         }
-        new_arr_[size_] = 0;
         delete[] arr_;
         arr_ = new_arr_;
         size_ = new_size_;
@@ -31,12 +30,11 @@ class MyArray{
         
     //Dicrement, (Dicrement == --)
     auto operator--(int) -> MyArray&{
-        const uint16_t new_size_ = size_ - 1;
-        uint16_t* new_arr_ = new uint16_t[new_size_];
+        const int new_size_ = size_ - 1;
+        int* new_arr_ = new int[new_size_];
         for(size_t i = 0 ; i < size_ ; i++){
             new_arr_[i] = arr_[i];
         }
-        new_arr_[size_] = 0;
         delete[] arr_;
         arr_ = new_arr_;
         size_ = new_size_;
@@ -44,21 +42,16 @@ class MyArray{
     }
     
     //Unary, (Unary == -)
-    auto operator-() -> MyArray&{
-        const uint16_t new_size_ = size_ - (size_ * size_);
-        uint16_t* new_arr_ = new uint16_t[new_size_];
-        for(size_t i = 0 ; i < size_ ; i++){
-            new_arr_[i] = arr_[i];
+    auto operator-() const -> MyArray {
+        MyArray result(size_);
+        for (size_t i = 0; i < size_; i++) {
+            result.arr_[i] = -arr_[i];
         }
-        new_arr_[size_] = 0;
-        delete[] arr_;
-        arr_ = new_arr_;
-        size_ = new_size_;
-        return *this;
+        return result;
     }
 
     //operator(+ ? - ? -- ? ++ ?)
-    explicit operator uint16_t() const{
+    explicit operator int() const{
         return size_; //return size vector copy
     }
     ~MyArray(){
@@ -67,11 +60,13 @@ class MyArray{
         size_ = 0;
     }
     private:
-    uint16_t size_;
-    uint16_t* arr_ = new uint16_t [size_];
+    int size_;
+    int* arr_ = new int [size_];
 };
 
 auto main()->int{
     std::println("SUS");
     return EXIT_SUCCESS;
 }
+//uint16_t это short, надо переделать
+//вернуть копию это надо создать обьект класса записать туда с противоположным значением.
